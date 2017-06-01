35e5cd1c0c1624c4465ae033878c27538e711459
RyanGunawardana <ryanmgunawardana@gmail.com>
2017-06-01 08:01:17 +0800

    Update install_lamp.sh

--- install_lamp.sh
+++ install_lamp.sh
@@ -9,6 +9,7 @@ sudo apt-get install -y apache2 php libapache2-mod-php;
 echo "INSTALLING MYSQL..."
 sudo apt-get install  mysql-server;
 echo "INSTALLING PHP5"
+sudo apt-get install -y php-xml;
 sudo apt-get install -y php5 libapache2-mod-php5;
 sudo apt-get install -y php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl;
 echo "INSTALLING OPEN SSH SERVER"
@@ -27,6 +28,7 @@ echo "INSTALLING MYSQL..."
 sudo apt-get install  mysql-server;
 echo "INSTALLING PHP7.0"
 sudo apt-get search php7.0;
+sudo apt-get install -y php-xml;
 sudo apt-get install -y php7.0 libapache2-mod-php7.0 phpmysqli;
 sudo apt-get install -y php-mcrypt php-gd php-mbstring;
 sudo apt-get install -y php7.0-mbstring php7.0-mysql php7.0-curl php7.0-json php7.0-mcrypt php7.0-xmlrpc;
